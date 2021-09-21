da006f996432297aa73a04bbff7873e83da90f26
unknown <rajputnikita5666@gmail.com>
2021-09-22 00:23:57 +0530

    Adding welcome program

---
+++ SnakeLadder.java
@@ -0,0 +1,9 @@
+package com.bridgelabz.snakeladder;
+
+public class SnakeLadder {
+
+	public static void main(String[] args) {
+		// Welcome Message
+		System.out.println("Welcome to Snake And Ladder program");
+	}
+}
