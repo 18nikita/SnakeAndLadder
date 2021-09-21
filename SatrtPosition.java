9c8bf4eee616639ea991d0bf29695aca2f1bbe07
unknown <rajputnikita5666@gmail.com>
2021-09-22 00:30:49 +0530

    Adding Start position of player

---
+++ SatrtPosition.java
@@ -0,0 +1,12 @@
+package com.bridgelabz.snakeladder;
+
+public class SatrtPosition {
+
+	public static void main(String[] args) {
+		// UC-1-Snake and Ladder game played with single player at start position 0
+		
+		int startPosition=0;
+		System.out.println("Player position at start is : "+startPosition);
+	}
+
+}
